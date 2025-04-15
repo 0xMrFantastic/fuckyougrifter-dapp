a98104c538859a58c054fd70798af800d88fb512
0xMrFantastic <oliinykyar@gmail.com>
2025-04-15 22:46:01 +0300

    Create Home.jsx

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to FuckYouGrifter.com 💀</h2>
+      <p>This app lets you publicly “award” Soulbound Tokens to known grifters and scammers.</p>
+      <p>Navigate using the links above to mint, explore awarded tokens, or view grifter status.</p>
+    </div>
+  )
+}
+
+export default Home
