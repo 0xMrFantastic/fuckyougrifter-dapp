f74bf18a889418ab9caf9327b25a6451486169e9
0xMrFantastic <oliinykyar@gmail.com>
2025-04-15 22:57:59 +0300

    Create Explorer.jsx

---
+++ src/pages/Explorer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Explorer = () => {
+  // In the future, this will display real on-chain recipients!
+  return (
+    <div>
+      <h2>Explorer</h2>
+      <p>This will display a list of all wallets that have received the FuckYouGrifter token.</p>
+    </div>
+  )
+}
+
+export default Explorer
