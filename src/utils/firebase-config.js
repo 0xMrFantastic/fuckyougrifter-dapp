9b38fecd33cbdfc895030d7e0bf431382e6ec8a3
0xMrFantastic <oliinykyar@gmail.com>
2025-04-17 17:01:50 +0300

    Create firebase-config.js

---
+++ src/utils/firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAB_2O3iR8eDjIAolcfouV5ArrYzjkv3Ac",
+  authDomain: "fuga-awards.firebaseapp.com",
+  projectId: "fuga-awards",
+  storageBucket: "fuga-awards.firebasestorage.app",
+  messagingSenderId: "310980832308",
+  appId: "1:310980832308:web:859ec0fb3c833ca8442e61"
+}
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+export { db }
