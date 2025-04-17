a90be0a13fb2d821c68a2926c34d50d9ddd075c9
0xMrFantastic <oliinykyar@gmail.com>
2025-04-17 14:34:41 +0000

    Integrate Firebase submit form + config

--- src/utils/firebase-config.js
+++ src/utils/firebase-config.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAB_2O3iR8eDjIAolcfouV5ArrYzjkv3Ac",
   authDomain: "fuga-awards.firebaseapp.com",
   projectId: "fuga-awards",
-  storageBucket: "fuga-awards.firebasestorage.app",
+  storageBucket: "fuga-awards.appspot.com",
   messagingSenderId: "310980832308",
   appId: "1:310980832308:web:859ec0fb3c833ca8442e61"
 }
